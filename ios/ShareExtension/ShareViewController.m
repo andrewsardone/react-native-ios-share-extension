131fa1db2e41eecdf06038e98f1a25de4ba89d35
Andrew Sardone <andrew@andrewsardone.com>
2015-11-29 12:46:20 -0500

    Use React Native view as ShareViewController's root view
    This is an attempt to display a “full screen” UI within our
    ShareViewController that's “server up” by React Native.

--- ios/ShareExtension/ShareViewController.m
+++ ios/ShareExtension/ShareViewController.m
@@ -1,10 +1,20 @@
 #import "ShareViewController.h"
+#import <RCTRootView.h>
 
 @implementation ShareViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    self.view.backgroundColor = [UIColor blueColor];
+- (void)loadView {
+    // hardcoding our JS to be served from the dev server while we're
+    // experimenting with the share extension setup
+    NSURL *jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle?platform=ios&dev=true"];
+
+    // attempt to initialize an RCTRootView and use it as the root view of our
+    // plain ‘ol UIViewController
+    RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
+                                                        moduleName:@"ShareExtensionExample"
+                                                 initialProperties:nil
+                                                     launchOptions:nil];
+    self.view = rootView;
 }
 
 @end
